c7a5214c1c9ae33fae0ef331bd6e60bc13525846
arthurxuzi <arthur.schulze@escola.pr.gov.br>
2024-08-29 07:51:12 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,1 +1,41 @@
+let cor;
+let posicaoHorizontal; // x
+let posicaoVertical; // y
 
+function setup() {
+  createCanvas(500, 400);
+   background(color("gray"));
+  cor = color(random(0,250), random(0,190), random(0,200));
+  posicaoHorizontal = 200;
+  posicaoVertical = 200;
+}
+
+
+function draw() {
+  
+  fill(cor);
+  circle(posicaoHorizontal,posicaoVertical,50);
+ 
+  
+  
+  if (mouseX < posicaoHorizontal){
+    posicaoHorizontal =  posicaoHorizontal - 1;
+  }
+  
+  if (mouseX > posicaoHorizontal){
+    posicaoHorizontal =  posicaoHorizontal + 1;
+  }
+  
+  if (mouseY < posicaoVertical){
+    posicaoVertical--;
+  }
+    
+  if (mouseY > posicaoVertical){
+    posicaoVertical++;
+  }
+  
+  if (mouseIsPressed){
+    cor = color(random(0,255), random(0,255), random(0,255), random(0,100));
+  }
+    
+}
